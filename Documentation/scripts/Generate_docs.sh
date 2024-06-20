f3a6e3e36e833ec28b7636a4447bc5d471d13ed7
Henry Bloch <63680706+gywn9081@users.noreply.github.com>
2024-06-20 13:07:48 -0500

    Create Documentation Builder

---
+++ Documentation/scripts/Generate_docs.sh
@@ -0,0 +1,14 @@
+# Must be run from home directory
+
+mkdir Documentation/Generated
+destination_directory="$(pwd)/Documentation/Generated"
+
+cd $(cat Documentation/targets)/src
+
+
+# Grab python files from packages in catkin directory
+for d in */src/*.py ; do
+    echo $(ls $d)
+    cp $(ls $d) "$destination_directory"
+done
+

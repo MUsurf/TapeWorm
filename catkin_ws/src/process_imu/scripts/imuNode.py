05c57636b5a8a606b311863748060e385e9548d0
spidious <lukedeffenbaugh@gmail.com>
2024-06-21 14:53:44 -0500

    Process the IMU Data
    I translated the quaternion to euler for the orientation

--- catkin_ws/src/process_imu/scripts/imuNode.py
+++ catkin_ws/src/process_imu/scripts/imuNode.py
@@ -7,11 +7,69 @@ Publishes:
 Subscribes:
         - /IMUdata
 
-Author: Luke Deffenbaugh
+Maintainer: Luke Deffenbaugh
 '''
 
 import rospy
 from sensor_msgs.msg import Imu
+from std_msgs.msg import String
+import numpy as np
+
+def quaternion_to_euler(x, y, z, w):
+    # Roll (x-axis rotation)
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = np.arctan2(sinr_cosp, cosr_cosp)
+
+    # Pitch (y-axis rotation)
+    sinp = 2 * (w * y - z * x)
+    if np.abs(sinp) >= 1:
+        pitch = np.sign(sinp) * (np.pi / 2)  # Use 90 degrees if out of range
+    else:
+        pitch = np.arcsin(sinp)
+
+    # Yaw (z-axis rotation)
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
+
+    return roll, pitch, yaw
+
+class imuData:
+    def __init__(self, data):
+        '''
+        Hold the data in specific formats
+
+        Orientation: Held in Euler format
+            0 : Roll
+            1 : Pitch
+            2 : Yaw
+
+        Angular Velocity: < Need to determine what its unit is >
+            0 : X
+            1 : Y
+            2 : Z
+
+        Linear Acceleration: < Need to determine what its unit is >
+            0 : X
+            1 : Y     
+            2 : Z   
+        '''
+
+        # Transform the data into Roll Pitch and Yaw
+        euler = quaternion_to_euler(data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w)
+
+        # Store the data into tuples
+        self.orientation = (euler[0], euler[1], euler[2])
+        self.angular_velocity = (data.angular_velocity.x, data.angular_velocity.y, data.angular_velocity.z)
+        self.linear_acceleration = (data.linear_acceleration.x, data.linear_acceleration.y, data.linear_acceleration.z)
+
+    def __repr__(self):
+        return (f"Orientation: {self.orientation[0]} {self.orientation[1]} {self.orientation[2]}\n"
+                f"Angular_Velocity: {self.angular_velocity[0]} {self.angular_velocity[1]} {self.angular_velocity[2]}\n"
+                f"Linear_Acceleration: {self.linear_acceleration[0]} {self.linear_acceleration[1]} {self.linear_acceleration[2]}\n")
+
+
 
 
 def imu_callback(data):
@@ -20,27 +78,11 @@ def imu_callback(data):
     This is the code that is called each time the subscriber receives data from the subscribed topic
     '''
 
-    # Log the orientation
-    rospy.loginfo("Orientation: x=%f, y=%f, z=%f, w=%f", 
-                  data.orientation.x, 
-                  data.orientation.y, 
-                  data.orientation.z, 
-                  data.orientation.w)
-    # Log the Angular Velocity
-    rospy.loginfo("Angular Velocity: x=%f, y=%f, z=%f", 
-                  data.angular_velocity.x, 
-                  data.angular_velocity.y, 
-                  data.angular_velocity.z)
-    # Log the linear acceleration
-    rospy.loginfo("Linear Acceleration: x=%f, y=%f, z=%f", 
-                  data.linear_acceleration.x, 
-                  data.linear_acceleration.y, 
-                  data.linear_acceleration.z)
+    # Initialize Object
+    imudata = imuData(data)
 
-    # TODO: Test that this works and learn to launch imu and process_imu nodes
-        # - Format data as necessary and publish to a more usable topic
-
-    # TODO: rename current imu to something more low level. This package (as far as the rest of our workspace is concerned) is imu
+    # Publish a string of the __repr__ function output
+    imu_string_publisher.publish(str(imudata))
 
 def imu_node():
     ''' 
@@ -51,6 +93,12 @@ def imu_node():
     rospy.init_node("imu_node", anonymous=True)
     # Setup Subscriber
     rospy.Subscriber("IMUdata", Imu, imu_callback)
+
+    # Create a publisher
+    global imu_string_publisher
+    imu_string_publisher = rospy.Publisher("ProcessedIMU", String, queue_size=10)
+
+
     # Keep the node running
     rospy.spin()
 

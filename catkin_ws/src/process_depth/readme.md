6a4167fb31cbf9dc00a977ac5b6ffd2781398c1c
Henry Bloch <63680706+gywn9081@users.noreply.github.com>
2024-07-26 20:48:21 -0500

    Add explanation and example to depth processing node

---
+++ catkin_ws/src/process_depth/readme.md
@@ -0,0 +1,7 @@
+# Purpose Statement
+
+This module is here to make adding a post processing step much easier when competition time arrives
+The thought behind this is there is alreday a partialy calibrated Kalman filter included in the depth sensor package
+adding our own smoothing function and other functions on top of the K-filter may give inconsisent results as well as heavily degraded preformance.
+
+## It does not currently contain anything but a framework for adding a smoothing function
--- catkin_ws/src/process_depth/scripts/depthNode.py
+++ catkin_ws/src/process_depth/scripts/depthNode.py
@@ -1,14 +1,53 @@
 #!/usr/bin/env python
 
 '''
-ROS 
+ROS
+---
 
 node: depth_node
-Publishes: 
-Subscribes:
-        - rov/ms5837
+-----
 
-Maintainer: Henry Bloch
+Publishes:
+----------
+        - depth_processed
+
+Subscribes:
+-----------
+        - rov/ms5837_filtered
+
 '''
 
+# Begin imports
 import rospy
+# End imports
+
+
+class SmoothFunction():
+    def __init__(self) -> None:
+        self.data: int = 0
+
+    def smooth(self, data: int) -> int:
+        return (data * 10 - 1 + 10 * 5) # for example only not doing anything
+    
+    def callback(self, message) -> None:
+        self.data = self.smooth(message.data)
+
+def commander():
+    smoother = SmoothFunction()
+
+    pub: rospy.Publisher = rospy.Publisher('depth_processed', int, queue_size=-1)
+    rospy.init_node('depth_processed', anonymous=True)
+    rate = rospy.Rate(.1) # 10hz
+
+    while not rospy.is_shutdown():
+        pub.publish(data=smoother.data)
+        rospy.loginfo(smoother.data)
+        rospy.Subscriber("rov/ms5837_filtered", int, smoother.callback)
+        rospy.spin()
+        # rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        commander()
+    except rospy.ROSInterruptException:
+        pass
